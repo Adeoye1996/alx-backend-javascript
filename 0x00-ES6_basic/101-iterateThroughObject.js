58ed95a9e81faa6cf16101f1696c0e4ff95e83a5
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-07-23 22:26:55 +0000

    advance

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,14 @@
+/* eslint-disable radix */
+export default function iterateThroughObject(reportWithIterator) {
+  let output = '';
+
+  for (const [index, item] of reportWithIterator.entries()) {
+    output += `${item}`;
+
+    if (parseInt(index) !== reportWithIterator.length - 1) {
+      output += ' | ';
+    }
+  }
+
+  return output;
+}
