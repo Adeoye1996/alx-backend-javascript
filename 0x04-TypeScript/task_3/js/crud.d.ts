866ba54472529ae0262c39974cda40a2daef86d6
Adeoye1996 <Abdulazeezbabatunde10@gmail.com>
2024-08-01 01:25:27 +0000

    task3,task4,task5

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,4 +1,4 @@
-iimport { RowID, RowElement } from "./interface";
+import { RowID, RowElement } from "./interface";
 
 export function insertRow(row: RowElement): number;
 export function deleteRow(rowId: RowID): void;
